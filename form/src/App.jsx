b6fb8b9a34eafef5ea9f108025322ea9637726e1
Subhabrata Mondal <78858011+Subhabrata0010@users.noreply.github.com>
2025-02-17 14:00:52 +0530

    managed file locations

---
+++ form/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import EmailLookup from "./components/EmailLookup";
+import Admin from "./components/Admin";
+import QRScanner from "./components/QRScanner";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <EmailLookup />
+              </>
+            }
+          />
+          <Route path="/Admin" element={<QRScanner />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
